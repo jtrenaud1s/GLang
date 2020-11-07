78f18c7e46a165d0b3b739362e57838d0125605f
Zach Philipp <mrphilipp7@gmail.com>
2020-11-06 20:58:23 -0600

    added multiplication and division binary operators (#42)

--- src/main/java/edu/semo/jatsz/glang/Lexer.java
+++ src/main/java/edu/semo/jatsz/glang/Lexer.java
@@ -159,8 +159,8 @@ public class Lexer implements Tokens {
 
     // load the next token
     public void next() {
-        final char[] c = { '=', '+', '-', ';', '(', ')' };
-        final int[] ct = { EQUAL, ADD, SUB, SEMI, LPAREN, RPAREN };
+        final char[] c = { '=', '+', '-', ';', '(', ')', '*', '/' };
+        final int[] ct = { EQUAL, ADD, SUB, SEMI, LPAREN, RPAREN, MULTIPLY, DIVIDE };
 
         // skip whitespace
         while (Character.isWhitespace(currentChar)) {
--- src/main/java/edu/semo/jatsz/glang/Tokens.java
+++ src/main/java/edu/semo/jatsz/glang/Tokens.java
@@ -1,4 +1,4 @@
-// Output created by jacc on Fri Nov 06 20:08:40 CST 2020
+// Output created by jacc on Fri Nov 06 20:43:22 CST 2020
 
 package edu.semo.jatsz.glang;
 
@@ -7,16 +7,18 @@ interface Tokens {
     int ADD = 1;
     int ALPHA = 2;
     int ALPHANUM = 3;
-    int DOUBLE = 4;
-    int EQUAL = 5;
-    int ID = 6;
-    int INT = 7;
-    int LITERAL = 8;
-    int LPAREN = 9;
-    int PRINT = 10;
-    int READ = 11;
-    int RPAREN = 12;
-    int SEMI = 13;
-    int SUB = 14;
-    int error = 15;
+    int DIVIDE = 4;
+    int DOUBLE = 5;
+    int EQUAL = 6;
+    int ID = 7;
+    int INT = 8;
+    int LITERAL = 9;
+    int LPAREN = 10;
+    int MULTIPLY = 11;
+    int PRINT = 12;
+    int READ = 13;
+    int RPAREN = 14;
+    int SEMI = 15;
+    int SUB = 16;
+    int error = 17;
 }
